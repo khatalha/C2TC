e01856fc929a589182d2e6ef9097e83205d9cf38
khatalha <khatalha2015@gmail.com>
2022-02-19 03:24:42 +0530

    First commit

--- C2TC/src/DateAndTime/dateapi.java
+++ C2TC/src/DateAndTime/dateapi.java
@@ -1,7 +1,10 @@
 package DateAndTime;
 
+import java.time.Clock;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 
 public class dateapi {
 
@@ -25,9 +28,32 @@ public class dateapi {
 		
 		LocalDate bdate= LocalDate.of(2002, 07, 13);
 		Period p= Period.between(date, bdate);
-		System.out.printf("You are %d Years %d month %d days  old",p.getYears(),p.getMonths(),p.getDays());
+		//System.out.printf("You are %d Years %d month %d days  old",p.getYears(),p.getMonths(),p.getDays());
 		
-
+		LocalDate localDate1 = LocalDate.now();
+		//System.out.println(localDate1);
+		
+		Clock clock = Clock.systemDefaultZone();
+		LocalDate localDate2 = LocalDate.now(clock);
+		//System.out.println(localDate2);
+		
+		ZoneId zoneId = ZoneId.of("Europe/Paris");
+		LocalDate localDate3 = LocalDate.now(zoneId);
+		//System.out.println(localDate3);
+		
+		System.out.println("--------------------------");
+		LocalDate localDate4 = LocalDate.of(2017, 03, 02);
+		//System.out.println(localDate4);
+		
+		LocalDate localDate5 = LocalDate.parse("2017-03-05");
+		
+		System.out.println(localDate5);
+		
+		LocalDate localDate6 = localDate5.plusDays(2);
+		//System.out.println(localDate6);
+		
+		LocalDate minus = localDate5.minus(2, ChronoUnit.DAYS);
+		//System.out.println(minus);
 	}
 
 }
